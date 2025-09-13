fa693cca6339aa7bc962302e4ff14d48362845d7
dmas5 <artur.unt@gmail.com>
2025-09-13 17:12:29 +0300

    REST API method renewed

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -7,6 +7,7 @@ from app.models.sentiment_analysis import sentiment_analysis
 bp = Blueprint('posts', __name__, url_prefix='/posts')
 
 
+# This GET method gets the posts from reddit api and returns analyzed data
 @bp.route('/<subreddit>/<type_subbreddit>/<int:count>', methods=['GET'])
 def get_posts_subreddit(subreddit,type_subbreddit,count):
     posts = asyncio.run(get_posts(subreddit,type_subbreddit,count))
