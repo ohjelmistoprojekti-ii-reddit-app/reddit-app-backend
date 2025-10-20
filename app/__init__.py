67a9d896be60946ad94cce180c9922c77e2e78d6
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-10-20 15:21:46 +0300

    Add API endpoints for subreddit options and update docs

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from app.routes.routes import bp as posts_bp
+from app.routes.subreddit_routes import bp as subreddit_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('app.config.Config')
     app.register_blueprint(posts_bp)
+    app.register_blueprint(subreddit_bp)
     
     return app
