63eed82ecf518640d4b5d388cc64438d5ff86d7c
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-10-25 11:50:06 +0300

    Update trending topics pipeline

--- scripts/topics_pipeline.py
+++ scripts/topics_pipeline.py
@@ -4,40 +4,40 @@ from app.models.topic_modeling import extract_topics
 from app.models.sentiment_analysis import sentiment_analysis
 from app.services.db import save_posts_to_database
 from app.config import Config
+import sys
 
-""""
+"""
 For GitHub Actions workflow:
 Fetches and analyzes posts from the given subreddits, and inserts the results into the database
+
+Usage: python -m scripts.topics_pipeline
 """
 
-def pipeline(subreddit):
+def topics_pipeline(subreddit):
     print(f"===== PROCESSING SUBREDDIT: {subreddit} =====")
-        
+    
     try:
         posts = asyncio.run(get_posts(subreddit, "hot", 500, 2))
-    except Exception as e:
-        print(f"::error::Error fetching posts: {e}")
-
-    try:
         topics = extract_topics(posts)
-    except Exception as e:
-        print(f"::error::Error extracting topics: {e}")
-
-    try:
         analyzed_topics = sentiment_analysis(topics)
-    except Exception as e:
-        print(f"::error::Error analyzing sentiment: {e}")
-
-    try:
         save_posts_to_database(analyzed_topics, subreddit, "posts")
+        return True
     except Exception as e:
-        print(f"::error::Error inserting into database: {e}")
+        print(f"::error::Pipeline failed for '{subreddit}': {e}")
+        return False
 
 if __name__ == "__main__":
     subreddits = Config.SUBREDDITS
 
+    failure = False
+
     for subreddit in subreddits:
-        try:
-            pipeline(subreddit)
-        except Exception as e:
-            print(f"::error::Pipeline failed for {subreddit}: {e}")
+        success = topics_pipeline(subreddit)
+        if success:
+            print(f"✔️  Successfully analyzed '{subreddit}'\n")
+        else:
+            failure = True
+
+    # Make sure that GitHub Actions workflow fails if any subreddit processing fails
+    if failure:
+        sys.exit(1)
