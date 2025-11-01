34098b4e5a90c95c637d2dec4d0426edd7c1dc76
dmas5 <artur.unt@gmail.com>
2025-11-02 01:10:17 +0200

    api names

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,23 @@ from flask import Flask
 from flask_cors import CORS
 from app.routes.statistics import statistics_bp
 from app.routes.topics import topics_bp
+from app.routes.reddit_api import reddit_api_bp
 from app.routes.subbreddits import subbreddits_bp
 from app.routes.countries import countries_bp
+# Old api file ready to be removed
+from app.routes.routes import bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('app.config.Config')
-    app.register_blueprint(topics_bp)
+    # Old api blueprint ready to be removed
+    app.register_blueprint(bp)
+    
     app.register_blueprint(subbreddits_bp)
     app.register_blueprint(countries_bp)
+    app.register_blueprint(topics_bp)
     app.register_blueprint(statistics_bp)
+    app.register_blueprint(reddit_api_bp)
     
     return app
