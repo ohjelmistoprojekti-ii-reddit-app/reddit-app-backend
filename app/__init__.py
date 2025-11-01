03a80562742412d8f08ad1a65a49cc09c120d596
dmas5 <artur.unt@gmail.com>
2025-11-02 00:55:58 +0200

    api names

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
-from app.routes.routes import bp as posts_bp
-from app.routes.subreddit_routes import bp as subreddit_bp
-from app.routes.countries_routes import bp as countries_bp
+from app.routes.statistics import statistics_bp
+from app.routes.topics import topics_bp
+from app.routes.subbreddits import subbreddits_bp
+from app.routes.countries import countries_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('app.config.Config')
-    app.register_blueprint(posts_bp)
-    app.register_blueprint(subreddit_bp)
+    app.register_blueprint(topics_bp)
+    app.register_blueprint(subbreddits_bp)
     app.register_blueprint(countries_bp)
+    app.register_blueprint(statistics_bp)
     
     return app
--- app/routes/countries.py
+++ app/routes/countries.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify
 from app.services.db import get_latest_posts_by_subreddit
 
-bp = Blueprint('countries', __name__, url_prefix='/countries')
+countries_bp = Blueprint('countries', __name__, url_prefix='/countries')
 
 # Get latest batch of analyzed data for a given country subreddit
-@bp.route('/latest/<subreddit>', methods=['GET'])
+@countries_bp.route('/latest/<subreddit>', methods=['GET'])
 def get_latest_country_data(subreddit):
     data = get_latest_posts_by_subreddit(subreddit, "countries")
 
