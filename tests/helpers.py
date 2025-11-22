d7c05e5081dff85923dc3aed2eb058db117ab20a
kkivilahti <bhm585@myy.haaga-helia.fi>
2025-11-22 18:20:07 +0200

    Add auth tests for login and register

--- tests/helpers.py
+++ tests/helpers.py
@@ -6,3 +6,15 @@ def get_country_subreddit(login_required):
             subreddit = item['subreddit']
             break
     return subreddit
+
+def register_user(client, username, email, password):
+    return client.post(
+        "/api/authentication/register",
+        json={"username": username,"email": email,"password": password}
+    )
+
+def login_user(client, username, password):
+    return client.post(
+        "/api/authentication/login",
+        json={"username": username,"password": password}
+    )
